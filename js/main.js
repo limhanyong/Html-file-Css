4610aaf4e567abbd95a22f9f1b4dcdec4cd4e2f5
Lim han yong <107594987+limhanyong@users.noreply.github.com>
2023-01-26 11:04:10 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
 // Whole-script strict mode syntax
 'use strict';
 console.log('hello World');
+// var (don't ever use this!)
+// var hoisting (move declaration from bottom to top) 항상 제일 위로 선언을 끌어 올려주는것
+// has no block scope
+// Contants
+// faver immutable data type always for a few reasons:
+// const daysinweek = 7; 변하지 않는 선언문.
+// variable type;
+// number 정수의 길이와 상관없이 선언
+// let a: number = 12;
+// let b: number = 1.2;
+// 
+const infinity = 1/0;
+const nAn = 'not a number' / 2;
+console.log('infinity');
+console.log('nAn');
+
